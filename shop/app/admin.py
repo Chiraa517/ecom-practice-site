059f063b904bfb0814d370b5443c49c9c269c4da
Chiraa517 <chiraastudio@gmail.com>
2023-01-31 11:58:51 +0500

    created all models

--- shop/app/admin.py
+++ shop/app/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from .models import Contry, City, Area, Customer
+from .models import Contry, City, Area, Customer, Category, Product, Order, OrderStatus, Cart
 # Register your models here.
 admin.site.register(Contry)
 admin.site.register(City)
 admin.site.register(Area)
+admin.site.register(Category)
 admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderStatus)
+admin.site.register(Cart)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category']
+
+    class Media:
+        js = ("https://cdn.tiny.cloud/1/pvc4ir3y3m926ajbtvymmuia4120k3zo0k19dalsyuv591c3/tinymce/6/tinymce.min.js",
+              "js/tinymce.js", )
+
+
+admin.site.register(Product, ProductAdmin)
--- shop/app/models.py
+++ shop/app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from autoslug import AutoSlugField
+
+
 # Create your models here.
 
 
@@ -15,7 +18,6 @@ class City(models.Model):
 
     def __str__(self):
         return self.name
-
 
 
 class Area(models.Model):
@@ -35,6 +37,62 @@ class Customer(models.Model):
     area = models.ForeignKey(Area, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
-
     def __str__(self):
         return self.name
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=150)
+    image = models.ImageField(upload_to="Category")
+    slug = AutoSlugField(populate_from='title', unique=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    selling_price = models.DecimalField(max_digits=10, decimal_places=3)
+    discounted_price = models.DecimalField(max_digits=10, decimal_places=3)
+    quantity = models.IntegerField(max_length=100)
+    brand = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="Products")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    slug = AutoSlugField(populate_from='title', unique=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class OrderStatus(models.Model):
+    status = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.status
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order_status = models.ForeignKey(OrderStatus, on_delete=models.CASCADE, default="Pending")
+    quantity = models.IntegerField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.quantity
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.quantity
